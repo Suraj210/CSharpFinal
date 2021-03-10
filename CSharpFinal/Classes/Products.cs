6630dfc887a8f2d9eafb1f64ab753cd4e65c2d98
Suraj210 <surajji@code.edu.az>
2021-03-10 09:01:49 -0800

    Props are created in Product class

--- CSharpFinal/Classes/Products.cs
+++ CSharpFinal/Classes/Products.cs
@@ -8,5 +8,16 @@ namespace CSharpFinal.Classes
 {
     class Products
     {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public enum Category { }
+        public int Number { get; set; }
+        public readonly int Id;
+        private static int _id = 0;
+        public Products()
+        {
+            _id++;
+            Id = _id++;
+        }
     }
 }
